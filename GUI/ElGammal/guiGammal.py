8cff8b8323e79179f55a1b20233d32192d552a94
JuanDa14Sa <coziestwharf@gmail.com>
2022-04-20 18:58:18 -0500

    Organizar código y texto guía

--- GUI/ElGammal/guiGammal.py
+++ GUI/ElGammal/guiGammal.py
@@ -27,11 +27,89 @@ def elGammalGUI(window):
 
     gammal=ElGammal()
 
-    # window = Tk()
+    def check_prime(n):
+        if n.isnumeric():
+            n=int(n)
+            return True if isprime(n) else False
+        else:
+            return False
 
-    # window.geometry("1191x692")
-    # window.configure(bg = "#E4E4EE")
+    def calc_password():
+        if check_prime(entry_1.get()) and entry_2.get().isnumeric() and entry_3.get().isnumeric():
+            gammal.setKey(int(entry_1.get()),int(entry_2.get()),int(entry_3.get()))
+            entry_2.delete(0,'end')
+            entry_3.delete(0,'end')
+            entry_2.insert(0,str(gammal.alpha))
+            entry_3.insert(0,str(gammal.private_a))
+            eTextBeta.set(str(gammal.beta))
+            eTextM.set(str(gammal.m))	
+        else:
+            messagebox.showwarning("", "    Ingrese valores primos válidos    ") 
 
+    def cipherer():
+        if entry_6.get(1.0,"end-1c").replace(' ','').replace('\n','').isalpha():
+            entry_7.config(state='normal')
+            entry_7.delete(1.0,"end-1c")
+            entry_7.insert(1.0,str(gammal.encrypt(entry_6.get(1.0,"end-1c"))))
+            # entry_7.config(state='disabled')
+
+        else:
+            messagebox.showwarning("", "    Ingrese un mensaje válido    ")
+
+    def decipherer():
+        if check_prime(entry_1.get()) and entry_2.get().isnumeric() and entry_3.get().isnumeric():
+            tmp=entry_7.get(1.0,"end-1c").replace(' ','').replace('\n','')
+            print(tmp)
+            entry_6.delete(1.0,"end-1c")
+            entry_6.insert(1.0,str(gammal.num_to_text(gammal.decrypt(ast.literal_eval(tmp)))))
+        else:
+            messagebox.showwarning("", "    Ingrese valores válidos    ")
+
+    def on_enterP(e):
+        if entry_1.get() == 'Inserte un número primo':
+            entry_1.config(fg='black')
+            entry_1.delete(0, 'end')
+
+    def on_leaveP(e):
+        if entry_1.get() == '':
+            entry_1.config(fg='#4d4f4e')
+            entry_1.insert(0, 'Inserte un número primo') 
+
+    def on_enterAlpha(e):
+        if entry_2.get() == 'Inserte un número entre 1 y p-1':
+            entry_2.config(fg='black')
+            entry_2.delete(0, 'end')
+
+    def on_leaveAlpha(e):
+        if entry_2.get() == '':
+            entry_2.config(fg='#4d4f4e')
+            entry_2.insert(0, 'Inserte un número entre 1 y p-1') 
+
+    def on_enterA(e):
+        if entry_3.get() == 'Inserte un número entre 1 y p-2':
+            entry_3.config(fg='black')
+            entry_3.delete(0, 'end')
+
+    def on_leaveA(e):
+        if entry_3.get() == '':
+            entry_3.config(fg='#4d4f4e')
+            entry_3.insert(0, 'Inserte un número entre 1 y p-2') 
+
+    def on_enterTextoClaro(e):
+        if entry_6.get("1.0",'end-1c').replace('\n','') == 'Inserte texto claro':
+           entry_6.delete('1.0', 'end-1c')
+
+    def on_leaveTextoClaro(e):
+        if entry_6.get("1.0",'end-1c').replace('\n','') == '':
+           entry_6.insert('1.0', 'Inserte texto claro')
+
+    def on_enterTextoEncr(e):
+        if entry_7.get("1.0",'end-1c').replace('\n','') == 'Inserte texto encriptado, por ejemplo, [(1,2),(3,4),(5,6)]':
+           entry_7.delete('1.0', 'end-1c')
+
+    def on_leaveTextoEncr(e):
+        if entry_7.get("1.0",'end-1c').replace('\n','') == '':
+           entry_7.insert('1.0', 'Inserte texto encriptado, por ejemplo, [(1,2),(3,4),(5,6)]')
 
     canvas = Canvas(
         window,
@@ -42,7 +120,7 @@ def elGammalGUI(window):
         highlightthickness = 0,
         relief = "ridge"
     )
-
+  
     canvas.place(x = 0, y = 28)
     canvas.create_rectangle(
         630.0,
@@ -68,7 +146,7 @@ def elGammalGUI(window):
         borderwidth=0,
         highlightthickness=0,
         command=lambda: [gammal.genKey(), entry_1.delete(0,'end'), entry_2.delete(0,'end'),entry_3.delete(0,'end'), entry_1.insert(0,str(gammal.p)), entry_2.insert(0,str(gammal.alpha)),
-        entry_3.insert(0,str(gammal.private_a)),eTextBeta.set(str(gammal.beta)),eTextM.set(str(gammal.m))],
+        entry_3.insert(0,str(gammal.private_a)),eTextBeta.set(str(gammal.beta)),eTextM.set(str(gammal.m)),entry_1.config(fg='black'),entry_2.config(fg='black'),entry_3.config(fg='black')],
         relief="flat"
     )
     button_1.place(
@@ -93,36 +171,7 @@ def elGammalGUI(window):
         width=207.0,
         height=65.0
     )
-    def check_prime(n):
-        if n.isnumeric():
-            n=int(n)
-            return True if isprime(n) else False
-        else:
-            return False
 
-    def calc_password():
-        if check_prime(entry_1.get()) and entry_2.get().isnumeric() and entry_3.get().isnumeric():
-            gammal.setKey(int(entry_1.get()),int(entry_2.get()),int(entry_3.get()))
-            entry_2.delete(0,'end')
-            entry_3.delete(0,'end')
-            entry_2.insert(0,str(gammal.alpha))
-            entry_3.insert(0,str(gammal.private_a))
-            eTextBeta.set(str(gammal.beta))
-            eTextM.set(str(gammal.m))	
-        else:
-            messagebox.showwarning("", "    Ingrese valores primos válidos    ") 
-
-
-
-
-    def decipherer():
-        if check_prime(entry_1.get()) and entry_2.get().isnumeric() and entry_3.get().isnumeric():
-            tmp=entry_7.get(1.0,"end-1c").replace(' ','').replace('\n','')
-            print(tmp)
-            entry_6.delete(1.0,"end-1c")
-            entry_6.insert(1.0,str(gammal.num_to_text(gammal.decrypt(ast.literal_eval(tmp)))))
-        else:
-            messagebox.showwarning("", "    Ingrese valores válidos    ")
     button_image_3 = PhotoImage(
         file=relative_to_assets("button_3.png"))
     button_3 = Button(
@@ -138,17 +187,6 @@ def elGammalGUI(window):
         width=277.0,
         height=65.0
     )
-
-
-    def cipherer():
-        if entry_6.get(1.0,"end-1c").replace(' ','').replace('\n','').isalpha():
-            entry_7.config(state='normal')
-            entry_7.delete(1.0,"end-1c")
-            entry_7.insert(1.0,str(gammal.encrypt(entry_6.get(1.0,"end-1c"))))
-            # entry_7.config(state='disabled')
-
-        else:
-            messagebox.showwarning("", "    Ingrese un mensaje válido    ")
 
     button_image_4 = PhotoImage(
         file=relative_to_assets("button_4.png"))
@@ -186,6 +224,11 @@ def elGammalGUI(window):
         height=57.0
     )
 
+    entry_1.bind("<FocusIn>", on_enterP)
+    entry_1.bind("<FocusOut>", on_leaveP)
+    entry_1.config(fg='#4d4f4e')
+    entry_1.insert('0', 'Inserte un número primo')
+
     canvas.create_text(
         42.0,
         118.0-28,
@@ -214,6 +257,10 @@ def elGammalGUI(window):
         width=489.0,
         height=57.0
     )
+    entry_2.bind("<FocusIn>", on_enterAlpha)
+    entry_2.bind("<FocusOut>", on_leaveAlpha)
+    entry_2.config(fg='#4d4f4e')
+    entry_2.insert('0', 'Inserte un número entre 1 y p-1')
 
     entry_image_3 = PhotoImage(
         file=relative_to_assets("entry_3.png"))
@@ -234,6 +281,10 @@ def elGammalGUI(window):
         width=489.0,
         height=57.0
     )
+    entry_3.bind("<FocusIn>", on_enterA)
+    entry_3.bind("<FocusOut>", on_leaveA)
+    entry_3.config(fg='#4d4f4e')
+    entry_3.insert('0', 'Inserte un número entre 1 y p-2')
 
     canvas.create_text(
         43.0,
@@ -335,6 +386,9 @@ def elGammalGUI(window):
         width=496.0,
         height=120.0,
     )
+    entry_6.bind("<FocusIn>", on_enterTextoClaro)
+    entry_6.bind("<FocusOut>", on_leaveTextoClaro)
+    entry_6.insert('1.0', 'Inserte texto claro') 
 
     canvas.create_text(
         43.0,
@@ -365,7 +419,9 @@ def elGammalGUI(window):
         width=489.0,
         height=120.0
     )
-
+    entry_7.bind("<FocusIn>", on_enterTextoEncr)
+    entry_7.bind("<FocusOut>", on_leaveTextoEncr)
+    entry_7.insert('1.0', 'Inserte texto encriptado, por ejemplo, [(1,2),(3,4),(5,6)]') 
     canvas.create_text(
         660.0,
         422.0-28,
